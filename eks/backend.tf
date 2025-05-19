b4e978a36d1bfd4060119be2e11bc69f11f6c903
SURENDAR <93500446+Mr-Surendar@users.noreply.github.com>
2025-05-19 14:37:35 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,20 +1,22 @@
 terraform {
   required_version = "~> 1.9.3"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.49.0"
     }
   }
+
   backend "s3" {
     bucket         = "dev-aman-tf-bucket"
-    region         = "us-east-1"
     key            = "eks/terraform.tfstate"
+    region         = "us-east-1"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
 }
 
 provider "aws" {
-  region  = var.aws-region
+  region = var.aws_region
 }
