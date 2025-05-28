fcb52e22b86dfb514edf70ffbafbf79202cbd581
Mr. SURENDAR <preepsurendar874@gmail.com>
2025-05-28 16:04:43 +0530

    after

--- module/vpc.tf
+++ module/vpc.tf
@@ -153,7 +153,7 @@ resource "aws_security_group" "eks-cluster-sg" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] // It should be specific IP range
+    cidr_blocks = ["98.84.167.13/32"] // It should be specific IP range
   }
 
   egress {
